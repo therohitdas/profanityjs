7a064aa0a4e8c6df99ab30207eb407a921605feb
Rohit Das <43847374+therohitdas@users.noreply.github.com>
2021-04-03 21:50:28 +0530

    Used chai's should for errors

--- test/test.js
+++ test/test.js
@@ -1,91 +1,177 @@
-var assert = require('assert');
-var { containsProfanity, maskProfanity } = require("../index")
+var should = require("chai").should();
+var { containsProfanity, maskProfanity } = require("../index");
 
-describe('Profanity checker', function () {
-    describe('#containsProfanity()', function () {
-        it('should return true for this sentence', function () {
-            assert.strictEqual(true, containsProfanity("tum madharchod ho", true));
-        });
+describe("Profanity checker", function () {
+  describe("#containsProfanity()", function () {
+    it("should return true for this sentence", function () {
+      var result = containsProfanity("tum madharchod ho", true);
+      result.should.equal(true);
     });
-    describe('#containsProfanity()', function () {
-        it('should return true for this sentence', function () {
-            assert.strictEqual(true, containsProfanity("lorem ipsum doler sit amet hot carl", true));
-        });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return true for this sentence", function () {
+      var result = containsProfanity(
+        "lorem ipsum doler sit amet hot carl",
+        true
+      );
+      result.should.equal(true);
     });
-    describe('#containsProfanity()', function () {
-        it('should return true for this sentence', function () {
-            assert.strictEqual(true, containsProfanity("lorem ipsum sås", true));
-        });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return true for this sentence", function () {
+      var result = containsProfanity("lorem ipsum sås", true);
+      result.should.equal(true);
     });
-    describe('#containsProfanity()', function () {
-        it('should return true for this sentence', function () {
-            assert.strictEqual(true, containsProfanity("lorem ipsum sit doler amet กระเด้า", true));
-        });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return true for this sentence", function () {
+      var result = containsProfanity(
+        "lorem ipsum sit doler amet กระเด้า",
+        true
+      );
+      result.should.equal(true);
     });
-    // false values
-    describe('#containsProfanity()', function () {
-        it('should return false for this sentence', function () {
-            assert.strictEqual(false, containsProfanity("tum random boo ho", true));
-        });
+  });
+  // false values
+  describe("#containsProfanity()", function () {
+    it("should return false for this sentence", function () {
+      var result = containsProfanity("tum random boo ho", true);
+      result.should.equal(false);
     });
-    describe('#containsProfanity()', function () {
-        it('should return false for this sentence', function () {
-            assert.strictEqual(false, containsProfanity("lorem ipsum doler sit amet", true));
-        });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return false for this sentence", function () {
+      var result = containsProfanity("lorem ipsum doler sit amet", true);
+      result.should.equal(false);
     });
-    describe('#containsProfanity()', function () {
-        it('should return false for this sentence', function () {
-            assert.strictEqual(false, containsProfanity("lorem ipsum rohit", true));
-        });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return false for this sentence", function () {
+      var result = containsProfanity("lorem ipsum rohit", true);
+      result.should.equal(false);
     });
-    describe('#containsProfanity()', function () {
-        it('should return false for this sentence', function () {
-            assert.strictEqual(false, containsProfanity("lorem ipsum sit doler amet sink", true));
-        });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return false for this sentence", function () {
+      var result = containsProfanity("lorem ipsum sit doler amet sink", true);
+      result.should.equal(false);
     });
+  });
+  // errors
+  describe("#containsProfanity()", function () {
+    it("should return error for empty statement", function () {
+      (function () {
+        containsProfanity("", true);
+      }.should.throw(
+        "containsProfanity(): first argument must be a non-empty string"
+      ));
+    });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return error for empty statement", function () {
+      (function () {
+        containsProfanity(1, true);
+      }.should.throw(
+        "containsProfanity(): first argument must be a non-empty string"
+      ));
+    });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return error for useHomoglyphSearch not passed", function () {
+      (function () {
+        containsProfanity("something");
+      }.should.throw(
+        "containsProfanity(): second argument must be a boolean value"
+      ));
+    });
+  });
+  describe("#containsProfanity()", function () {
+    it("should return error for useHomoglyphSearch not boolean", function () {
+      (function () {
+        containsProfanity("something", 1);
+      }.should.throw(
+        "containsProfanity(): second argument must be a boolean value"
+      ));
+    });
+  });
 });
 
-
-describe('Profanity Masker', function () {
-    describe('#maskProfanity()', function () {
-        it('should mask profanity for this sentence', function () {
-            assert.strictEqual("tum ********** ho", maskProfanity("tum madharchod ho", true));
-        });
+describe("Profanity Masker", function () {
+  describe("#maskProfanity()", function () {
+    it("should mask profanity for this sentence with *", function () {
+      var result = maskProfanity("tum madharchod ho", "*");
+      result.should.equal("tum ********** ho");
     });
-    describe('#maskProfanity()', function () {
-        it('should mask profanity for this sentence', function () {
-            assert.strictEqual("lorem ipsum doler sit amet ********", maskProfanity("lorem ipsum doler sit amet hot carl", true));
-        });
+  });
+  describe("#maskProfanity()", function () {
+    it("should mask profanity for this sentence with *", function () {
+      var result = maskProfanity("lorem ipsum doler sit amet hot carl");
+      result.should.equal("lorem ipsum doler sit amet ********");
     });
-    describe('#maskProfanity()', function () {
-        it('should mask profanity for this sentence', function () {
-            assert.strictEqual("lorem ipsum ***", maskProfanity("lorem ipsum sås", true));
-        });
+  });
+  describe("#maskProfanity()", function () {
+    it("should mask profanity for this sentence with #", function () {
+      var result = maskProfanity("lorem ipsum sås", "#");
+      result.should.equal("lorem ipsum ###");
     });
-    describe('#maskProfanity()', function () {
-        it('should mask profanity for this sentence', function () {
-            assert.strictEqual("lorem ipsum sit doler amet *******", maskProfanity("lorem ipsum sit doler amet กระเด้า", true));
-        });
+  });
+  describe("#maskProfanity()", function () {
+    it("should mask profanity for this sentence", function () {
+      var result = maskProfanity("lorem ipsum sit doler amet กระเด้า", "*");
+      result.should.equal("lorem ipsum sit doler amet *******");
     });
-    // false values
-    describe('#maskProfanity()', function () {
-        it('should return the sentence unchanged', function () {
-            assert.strictEqual("tum random boo ho", maskProfanity("tum random boo ho", true));
-        });
+  });
+  // false values
+  describe("#maskProfanity()", function () {
+    it("should return the sentence unchanged", function () {
+      var result = maskProfanity("tum random boo ho", "*");
+      result.should.equal("tum random boo ho");
     });
-    describe('#maskProfanity()', function () {
-        it('should return the sentence unchanged', function () {
-            assert.strictEqual("lorem ipsum doler sit amet", maskProfanity("lorem ipsum doler sit amet", true));
-        });
+  });
+  describe("#maskProfanity()", function () {
+    it("should return the sentence unchanged", function () {
+      var result = maskProfanity("lorem ipsum doler sit amet", "*");
+      result.should.equal("lorem ipsum doler sit amet");
     });
-    describe('#maskProfanity()', function () {
-        it('should return the sentence unchanged', function () {
-            assert.strictEqual("lorem ipsum rohit", maskProfanity("lorem ipsum rohit", true));
-        });
+  });
+  describe("#maskProfanity()", function () {
+    it("should return the sentence unchanged", function () {
+      var result = maskProfanity("lorem ipsum rohit", "*");
+      result.should.equal("lorem ipsum rohit");
     });
-    describe('#maskProfanity()', function () {
-        it('should return the sentence unchanged', function () {
-            assert.strictEqual("lorem ipsum sit doler amet sink", maskProfanity("lorem ipsum sit doler amet sink", true));
-        });
+  });
+  describe("#maskProfanity()", function () {
+    it("should return the sentence unchanged", function () {
+      var result = maskProfanity("lorem ipsum sit doler amet sink", "*");
+      result.should.equal("lorem ipsum sit doler amet sink");
     });
+  });
+  // errors
+  describe("#maskProfanity()", function () {
+    it("should throw error for empty sentence", function () {
+      (function () {
+        maskProfanity("", "*");
+      }.should.throw(
+        "maskProfanity(): first argument must be a non-empty string"
+      ));
+    });
+  });
+  describe("#maskProfanity()", function () {
+    it("should throw error for mask too long", function () {
+      (function () {
+        maskProfanity("not empty sentence", "**");
+      }.should.throw(
+        "maskProfanity(): second argument must be a single char. Default - *"
+      ));
+    });
+  });
+  describe("#maskProfanity()", function () {
+    it("should throw error for mask too short", function () {
+      (function () {
+        maskProfanity("not empty sentence", "");
+      }.should.throw(
+        "maskProfanity(): second argument must be a single char. Default - *"
+      ));
+    });
+  });
 });
